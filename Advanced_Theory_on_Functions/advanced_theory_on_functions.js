89930a4a212a376d1ce2ceadbe28c0786b064f65
Bananchik7 <zykovast@mail.ru>
2023-02-19 20:38:48 +0300

    finish version

--- Advanced_Theory_on_Functions/advanced_theory_on_functions.js
+++ Advanced_Theory_on_Functions/advanced_theory_on_functions.js
@@ -169,26 +169,8 @@ console.log(result1);
 let arr1 = [1, 2, 3, 4, 5];
 function array(arr) {
   console.log(arr.shift());
-
   if (arr.length != 0) {
     array(arr);
   }
 }
 array(arr1);
-
-// С помощью рекурсии выведите все примитивные элементы этого объекта в консоль.
-let obj1 = {
-  a: 1,
-  b: { c: 2, d: 3, e: 4 },
-  f: { g: 5, j: 6, k: { l: 7, m: { n: 8, o: 9 } } },
-};
-function obj(obj) {
-  for (let key of obj) {
-    if (typeof elem == "object") {
-      obj(elem);
-    } else {
-      console.log(elem);
-    }
-  }
-}
-obj(obj1);
