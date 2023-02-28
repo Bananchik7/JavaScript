9a90515c99e5e71896df9c77b6d04e5e780f2333
Bananchik7 <zykovast@mail.ru>
2023-02-28 22:19:25 +0300

    28.02.2023

--- Introduction_to_DOM/introduction_to_DOM.js
+++ Introduction_to_DOM/introduction_to_DOM.js
@@ -7,4 +7,125 @@ console.log(elem2);
 let elem3 = document.querySelector("#elem3");
 console.log(elem3);
 
+// Получите ссылку на первый абзац из дива с id, равным block.
+let elem = document.querySelector("#block");
+console.log(elem);
+
+// Сделайте так, чтобы по клику на первую кнопку на экран выводилось число 1, по клику на вторую - число 2, а по клику на третью - число 3.
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+button1.addEventListener("click", function () {
+  console.log("1");
+});
+button2.addEventListener("click", function () {
+  console.log("2");
+});
+button3.addEventListener("click", function () {
+  console.log("3");
+});
+
+// Сделайте так, чтобы по клику на первую кнопку выполнилась функция func1, а по клику на вторую - функция func2.
+let button4 = document.querySelector("#button4");
+let button5 = document.querySelector("#button5");
+button4.addEventListener("click", func1);
+button5.addEventListener("click", func2);
+function func1() {
+  console.log(1);
+}
+function func2() {
+  console.log(2);
+}
+
+// Привяжите эту функцию ко всем 5-ти абзацам.
+let elem4 = document.querySelector("#elem4");
+let elem5 = document.querySelector("#elem5");
+let elem6 = document.querySelector("#elem6");
+let elem7 = document.querySelector("#elem7");
+let elem8 = document.querySelector("#elem8");
+elem4.addEventListener("click", func3);
+elem5.addEventListener("click", func3);
+elem6.addEventListener("click", func3);
+elem7.addEventListener("click", func3);
+elem8.addEventListener("click", func3);
+function func3() {
+  console.log("message");
+}
+
+// Дана кнопка. По двойному клику по ней выведите какое-нибудь сообщение.
+let button6 = document.querySelector("#button6");
+button6.addEventListener("dblclick", function () {
+  console.log("Двойной клик");
+});
+
+// По наведению на нее выведите какое-нибудь сообщение.
+button6.addEventListener("mouseover", function () {
+  console.log("Наведение");
+});
+
+// По уходу курсора с нее выведите какое-нибудь сообщение.
+button6.addEventListener("mouseout", function () {
+  console.log("Уход");
+});
+
+// Дан абзац и кнопка. По клику на кнопку прочитайте текст абзаца и выведите его в консоль.
+let button7 = document.querySelector("#button7");
+let elem9 = document.querySelector("#elem9");
+button7.addEventListener("click", function () {
+  console.log(elem9.textContent);
+});
+
+// Дан абзац и кнопка. По клику на кнопку запишите в абзац новый текст.
+button7.addEventListener("click", function () {
+  console.log((elem9.textContent = "!!!"));
+});
+
+// Даны два абзаца, содержащие своим текстом какие-то числа, и кнопка. По нажатию на кнопку выведите на экран сумму хранящихся чисел.
+let button8 = document.querySelector("#button8");
+let elem10 = document.querySelector("#elem10");
+let elem11 = document.querySelector("#elem11");
+button8.addEventListener("click", function () {
+  console.log(Number(elem10.textContent) + Number(elem11.textContent));
+});
+
+// Даны три абзаца с числами, див, и кнопка. По нажатию на кнопку запишите в текст дива сумму чисел.
+let button9 = document.querySelector("#button9");
+let elem12 = document.querySelector("#elem12");
+let elem13 = document.querySelector("#elem13");
+let elem14 = document.querySelector("#elem14");
+let element = document.querySelector("#element");
+button9.addEventListener("click", function () {
+  element.textContent =
+    Number(elem12.textContent) +
+    Number(elem13.textContent) +
+    Number(elem14.textContent);
+});
+
+// Дан абзац и кнопка. По клику на кнопку прочитайте HTML код абзаца и выведите его в консоль.
+let elem15 = document.querySelector("#elem15");
+let button10 = document.querySelector("#button10");
+button10.addEventListener("click", function () {
+  console.log(elem15.innerHTML);
+});
+
+// Дана также кнопка. По нажатию на кнопку выведите на экран содержимое атрибута type указанного выше инпута.
+let elem16 = document.querySelector("#elem16");
+let button11 = document.querySelector("#button11");
+button11.addEventListener("click", function () {
+  console.log(elem16.type);
+});
+
+// Дана также кнопка. По нажатию на кнопку запишите в атрибут type значение submit.
+button11.addEventListener("click", function () {
+  console.log((elem16.type = "submit"));
+});
+
+// Пусть у вас есть картинка. Разместите ее на странице с помощью тега img. Пусть даны также кнопка и абзац. Сделайте так, чтобы по клику на кнопку в абзац записался путь к картинки из ее атрибута src.
+let elem17 = document.querySelector("#elem17");
+let button12 = document.querySelector("#button12");
+let image = document.querySelector("#image");
+button12.addEventListener("click", function () {
+  console.log((elem16.type = image.src));
+});
+
 //
